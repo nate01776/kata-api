ddc2c50eafe3abc08bc77146a0056edf92507a5c
Nathan Wright <nathan.wright@smartbear.com>
2018-08-08 17:05:27 -0400

    fixed test

--- test/models/pet_test.rb
+++ test/models/pet_test.rb
@@ -38,7 +38,7 @@ class PetTest < ActiveSupport::TestCase
     # fixtures :pets
 
     def test_create
-        pet = Pet.create(:name => "Trixie")
+        pet = Pet.create(:name => "Trixie", :status => "available")
         assert_equal "Trixie", pet.name
     end
 end

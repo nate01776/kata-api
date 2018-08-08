30bba998cefc6485abecd38dcea807ca76104769
Nathan Wright <nathan.wright@smartbear.com>
2018-08-08 15:13:45 -0400

    rackup looks good

--- config.ru
+++ config.ru
@@ -1,4 +1,4 @@
-$stdout.repoen('./logs/log_file.txt', 'a')
+$stdout.reopen('./logs/log_file.txt', 'a')
 $stdout.sync = true
 
 require File.expand_path('server.rb', File.dirname(__FILE__))
--- start.rb
+++
@@ -1,9 +0,0 @@
-$stdout.repoen('./logs/log_file.txt', 'a')
-$stdout.sync = true
-
-loop.with_index do |_, i|
-    puts i
-    sleep(3)
-end
-
-# nohup ruby ./server.rb -p 4567 -o 10.0.25.247 >> ./logs/log_file.txt 2>&1 &
